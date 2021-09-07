ab3b93e3af75b6d00f2578d88ae33eab80a7461a
architgarg13 <69807940+architgarg13@users.noreply.github.com>
2021-09-07 22:03:00 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+        domains: ['links.papareact.com',],
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1IjoiYXJjaGl0Z2FyZzEzIiwiYSI6ImNrdDlxc2hybzA0ancydms0a253YXN0aTQifQ.H34VHdPORwU187siM24oog',
+    },
+};
